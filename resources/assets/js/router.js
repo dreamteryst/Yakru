146a6ec5c74ad13929a65dfc72dd4640e345445c
dreamteryst <dreamt451@gmail.com>
2018-04-19 12:47:01 +0700

    add product detail page

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -1,11 +1,13 @@
 import VueRouter from 'vue-router';
 import Home from './components/Home';
+import ProductDetail from './components/ProductDetail';
 
 window.Vue = require('vue');
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', component: Home }
+    { path: '/', component: Home },
+    { path: '/product-detail', component: ProductDetail}
 ]
 
 const router = new VueRouter({ mode: 'history', routes: routes});
